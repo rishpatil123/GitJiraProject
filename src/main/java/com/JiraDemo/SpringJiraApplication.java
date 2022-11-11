ffc6a65df03f817a0f10d36f57ac30c4af399988
rishpatil123 <rishpatil123@gmail.com>
2022-11-11 16:26:09 +0530

    SpringJiraProject

---
+++ src/main/java/com/JiraDemo/SpringJiraApplication.java
@@ -0,0 +1,13 @@
+package com.JiraDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JiraDemo/SpringJiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JiraDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
